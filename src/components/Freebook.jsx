67ce5029b50bc4e970a03f679b18492a141669ec
santhosh <santhsohspartan181@gmail.com>
2024-10-26 12:25:01 +0530

    Add Backend and Frontend files

---
+++ src/components/Freebook.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+import axios from "axios";
+
+import Cards from "./Cards";
+
+function Freebook() {
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        const data = res.data.filter((data) => data.category === "Free");
+        console.log(data);
+        setBook(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-6 my-12">
+        <div className="text-center md:text-left mb-8">
+          <h1 className="font-semibold text-2xl md:text-3xl pb-4">
+            Free Offered Courses
+          </h1>
+          <p className="text-gray-600 leading-relaxed md:w-3/4 mx-auto md:mx-0">
+            Explore our free selection of courses. Whether you're looking to
+            learn a new skill, explore a hobby, or dive into a completely new
+            field, we offer something for everyone. Start learning today!
+          </p>
+        </div>
+
+        <div className="mt-8">
+          <Slider {...settings}>
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Freebook;
